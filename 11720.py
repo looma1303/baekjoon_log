058ea5e8fe8cf8450c8ce7780617aa63d9f7174c
dltem <kmj13033@gmail.com>
2023-02-21 11:53:18 +0900

    Create 11720.py

---
+++ 11720.py
@@ -0,0 +1,13 @@
+a = list()
+b = list()
+for x in range(0,2):
+    n = input()
+    a.append(int(n))
+
+    
+result = 0
+for z in str(a[1]):
+    result = result + int(z)
+    
+print(result)
+    
