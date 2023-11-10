6969d2cb0a8f673c09e9b53a91c0cdde16b6cc17
dltem <31087952+looma1303@users.noreply.github.com>
2023-11-10 23:50:54 +0900

    Update 2941.py

--- 2941.py
+++ 2941.py
@@ -1,80 +1,141 @@
 n = input()
 n = list(n)
 del_pos = list()
-cro_apl_list = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+split_list = list()
 
 
     
     
-def del_str(a):
+def split_str(a):
+    global split_list
     global q
     global n
     for x in range(0, len(n)):
-        print(x)
-        if n[x] == 'c':
+        if len(n) == 1:
+            split_list.append(str(n[x]))
+            del n[x]
+            q = q + 1
+            break
+            
+        elif n[x] == 'c':
             if n[x + 1] == '=' or n[x + 1] == '-':
-                del n[x+1]
+                split_list.append(str(n[x])+str(n[x+1]))
+                del n[x]
+                del n[x]
                 q = q + 1
                 break
             else:
-                pass
+                split_list.append(str(n[x]))
+                del n[x]
+                q = q + 1
+                break
+            
+            
         elif n[x] == 'd':
-                if n[x + 1] == 'z':
-                    if n[x + 2] == '=':
-                        del n[x + 1]
-                        del n[x + 1]
-                        q = q + 1
-                        break 
-                    else:
-                        pass
-                elif n[x + 1] == '-':
-                    del n[x + 1]
+            if n[x + 1] == 'z':
+                if len(n) == 2:
+                    split_list.append(str(n[x]))
+                    del n[x]
                     q = q + 1
                     break
+                    
+                elif n[x + 2] == '=':
+                    split_list.append(str(n[x])+str(n[x+1])+str(n[x+2]))
+                    del n[x]
+                    del n[x]
+                    del n[x]
+                    q = q + 1
+                    break 
                 else:
-                    pass
+                    split_list.append(str(n[x]))
+                    del n[x]
+                    q = q + 1
+                    break
+                
+                
+            elif n[x + 1] == '-':
+                split_list.append(str(n[x])+str(n[x+1]))
+                del n[x]
+                del n[x]
+                q = q + 1
+                break
+            else:
+                split_list.append(str(n[x]))
+                del n[x]
+                q = q + 1
+                break
                     
         elif n[x] == 'l':
             if n[x + 1] == 'j':
-                del n[x + 1]
+                split_list.append(str(n[x])+str(n[x + 1]))
+                del n[x]
+                del n[x]
                 q = q + 1 
                 break
             else:
-                pass
+                split_list.append(str(n[x]))
+                del n[x]
+                q = q + 1
+                break
             
                         
         elif n[x] == 'n':
             if n[x + 1] == 'j':
-                del n[x + 1]
+                split_list.append(str(n[x]+str(n[x+1])))
+                del n[x]
+                del n[x]
                 q = q + 1 
                 break
             else:
-                pass
+                split_list.append(str(n[x]))
+                del n[x]
+                q = q + 1
+                break
                             
         elif n[x] == 's':
             if n[x + 1] == '=':
-                del n[x + 1]
+                split_list.append(str(n[x])+str(n[x + 1]))
+                del n[x]
+                del n[x]
                 q = q + 1 
                 break
+            else:
+                split_list.append(str(n[x]))
+                del n[x]
+                q = q + 1
+                break
+                
+                
         
         elif n[x] == 'z':
             if n[x + 1] == '=':
-                del n[x + 1]
+                split_list.append(str(n[x])+str(n[x + 1]))
+                del n[x]
+                del n[x]
                 q = q + 1
                 break
             else:
-                pass
+                split_list.append(str(n[x]))
+                del n[x]
+                q = q + 1
+                break
             
         else:
-            pass
+            split_list.append(str(n[x]))
+            del n[x]
+            q = q + 1
+            break
         
     return q
-        
+
+
 
 q = 1
 a = 0
+
 while q == 1:
     q = 0
-    q = del_str(a)
+    split_str(a)
     
-print(len(n))
+print(len(split_list))
+    
