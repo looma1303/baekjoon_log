53e78db8420187db3753803af5e02d8d97c5813e
looma1303 <kmj13033@gmail.com>
2025-06-10 06:30:01 +0000

    250610

--- 10989.py
+++ 10989.py
@@ -1,11 +1,10 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-data = list()
-for x in range(0,n):
-    q = int(input())
-    data.append(q)
-
-data.sort()
-for x in data:
-    print(x)
+count = [0] * (10001)  
+for _ in range(n):
+    num = int(input())
+    count[num] += 1 #이떄 오름차순으로 이미 정렬됨
+for i in range(1, 10001):
+    for _ in range(count[i]): #숫자가 저장되어있는 개수만큼 반복해서 출력
+        print(i)
